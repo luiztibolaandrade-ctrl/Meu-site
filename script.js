5d7e80461f485d0ebf8b8fce91ef162d7554a2c7
luiztibolaandrade-ctrl <luiz.tibola.andrade@escola.pr.gov.br>
2026-06-17 10:57:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+function mostrarPalpite() {
+    // Procura o elemento de texto onde o palpite vai aparecer
+    const textoPalpite = document.getElementById("palpite-luta");
+    
+    // Altera o texto dele
+    textoPalpite.innerText = "🔥 Nosso palpite: Vitória do mcgregor por Nocaute no 1º round!";
+}
